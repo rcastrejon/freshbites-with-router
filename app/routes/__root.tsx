2b67fffa0239a3ad66ff621c9a8de83f4ee7456d
Rodrigo Castrejon <me@rcastrejon.dev>
2024-11-15 09:23:19 -0600

    initial commit

---
+++ app/routes/__root.tsx
@@ -0,0 +1,44 @@
+// app/routes/__root.tsx
+import { createRootRoute } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
+import { Meta, Scripts } from "@tanstack/start";
+import * as React from "react";
+
+export const Route = createRootRoute({
+  meta: () => [
+    {
+      charSet: "utf-8",
+    },
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+    {
+      title: "TanStack Start Starter",
+    },
+  ],
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head>
+        <Meta />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
